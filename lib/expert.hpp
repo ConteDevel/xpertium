4ea27ab4e20458918cdfe4ccdc6636eeb6972af1
Denis Sologub <contedevel2010@gmail.com>
2019-01-25 05:01:34 +0300

    Fix bugs in the reverese output

--- lib/expert.hpp
+++ lib/expert.hpp
@@ -17,7 +17,7 @@ class expert_t {
     const base_dialog_t<val_t> &m_dialog;
     base_tracer_t<val_t> &m_tracer;
     vals_t<val_t> m_facts;
-    std::vector<rule_t<val_t> *> m_unused_rules;
+    std::vector<rule_t<val_t> *> m_cur_rules;
 public:
     /**
      * @brief Constructor
@@ -49,10 +49,10 @@ public:
      * @param init Initial facts
      */
     void reset(const std::vector<val_t> *init = nullptr) {
-        m_unused_rules.clear();
+        m_cur_rules.clear();
         auto rules = m_kb->rules();
         std::for_each(rules->begin(), rules->end(), [this] (const auto &p) {
-            m_unused_rules.push_back(p.get());
+            m_cur_rules.push_back(p.get());
         });
         m_facts.clear();
         m_tracer.clear();
@@ -65,7 +65,7 @@ public:
     }
 
     /**
-     * @brief Launch the expert system with reverse output
+     * @brief Launch the expert system with the reverse output
      * @param target_fact The target fact (`nullptr` to run for any target)
      * @return True if the target was achieved
      */
@@ -81,26 +81,26 @@ public:
     }
 
     /**
-     * @brief Launch the expert system with direct output
+     * @brief Launch the expert system with the direct output
      * @param target_fact The target fact (`nullptr` to run for any target)
      * @return True if the target was achieved
      */
     bool direct(const val_t *target_fact = nullptr) {
-        while (!m_unused_rules.empty()) {
-            auto it = m_unused_rules.begin();
-            auto old_size = m_unused_rules.size();
-            bool target = false;
-            for (; it < m_unused_rules.end(); ++it) {
-                target = false;
+        while (!m_cur_rules.empty()) {
+            auto it = m_cur_rules.begin();
+            auto old_size = m_cur_rules.size();
+            bool is_target = false;
+            for (; it < m_cur_rules.end(); ++it) {
+                is_target = false;
                 if ((*it)->is(m_facts)) {
                     if (!handle_rule(*it)) { return false; }
-                    target = (*it)->target();
+                    is_target = (*it)->target();
 
-                    m_unused_rules.erase(it);
+                    m_cur_rules.erase(it);
                 }
-                if (target) { break; }
+                if (is_target) { break; }
             }
-            if (target && !target_fact) {
+            if (is_target && !target_fact) {
                 m_dialog.print() << "Result: " << m_facts.back()
                                  << std::endl;
                 return true;
@@ -108,7 +108,7 @@ public:
                 m_dialog.print() << "Target was found!" << std::endl;
                 return true;
             }
-            if (old_size == m_unused_rules.size()) {
+            if (old_size == m_cur_rules.size()) {
                 if (target_fact) {
                     m_dialog.print() << "Target wasn't reached" << std::endl;
                 } else {
@@ -122,6 +122,11 @@ public:
     }
 
 private:
+    /**
+     * @brief Handles the rule to get its output and update fact database
+     * @param rule Rule
+     * @return False if the rule is invalid
+     */
     bool handle_rule(const rule_t<val_t> *rule) {
         val_t fact;
 
@@ -145,25 +150,69 @@ private:
         return true;
     }
 
-    bool reverse_impl(const val_t target_fact) {
-        for (auto i = m_unused_rules.begin(); i < m_unused_rules.end(); ++i) {
-            if ((*i)->is_possible_out(target_fact)) {
-                if (check_rule(*i, target_fact)) {
-                    auto uks = (*i)->unknowns(m_facts);
-                    if (uks.empty()) {
-                        return (*i)->is(m_facts);
-                    }
-                    for (auto u = uks.begin(); u != uks.end(); ++u) {
-                        if (reverse_impl(*u)) { return true; }
-                    }
-                }
+    bool reverse_impl(const val_t tgt_fact) {
+        for (auto i = m_cur_rules.begin(); i < m_cur_rules.end(); ++i) {
+            auto rule = *i;
+            if ((*i)->is_possible_out(tgt_fact) && check_output(*i, tgt_fact)) {
+                m_cur_rules.erase(i);
+                return prove_rule(rule, tgt_fact);
             }
         }
 
         return false;
     }
 
+    /**
+     * @brief Check if activation conditions of the rule are true
+     * @param rule Rule
+     * @param target_fact Target fact (it will be accepted as true fact if the
+     *                    conditions are true)
+     * @return Check result
+     */
     bool check_rule(const rule_t<val_t> *rule, val_t target_fact) {
+        if (rule->is(m_facts)) {
+            m_facts.push_back(target_fact);
+            m_tracer.push_fact(target_fact);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @brief Try to prove that the target is a fact
+     * @param rule Rule that contains a required output
+     * @param target_fact Target (potential fact)
+     * @return True if the target was proved
+     */
+    bool prove_rule(const rule_t<val_t> *rule, val_t target_fact) {
+        bool approved;
+        do {
+            approved = false;
+            auto uks = rule->unknowns(m_facts);
+
+            if (uks.empty()) { return check_rule(rule, target_fact); }
+
+            for (auto u = uks.begin(); u != uks.end(); ++u) {
+                if (reverse_impl(*u)) {
+                    approved = true;
+
+                    break;
+                }
+            }
+        } while (approved);
+
+        return false;
+    }
+
+    /**
+     * @brief Check if the rule will produce a required output fact
+     * @param rule Rule
+     * @param target_fact Target fact
+     * @return Check result
+     */
+    bool check_output(const rule_t<val_t> *rule, val_t target_fact) {
         val_t fact;
 
         if (rule->question()) {
@@ -172,14 +221,13 @@ private:
             fact = *rule->out();
         } else {
             m_dialog.print() << "Rule `" << rule->id()
-                              << "` doesn't consist question or output"
-                              << std::endl;
+                             << "` doesn't consist question or output"
+                             << std::endl;
 
             return false;
         }
 
         m_tracer.push_rule(rule, fact);
-        m_tracer.push_fact(fact);
 
         return fact == target_fact;
     }
